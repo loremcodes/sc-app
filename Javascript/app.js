31c65fc0407746107eaeafe416a058935cdbe31b
loremcodes <prasadjunghare2@gmail.com>
2023-02-23 14:01:16 +0530

    added a new images grid

--- Javascript/app.js
+++ Javascript/app.js
@@ -57,7 +57,7 @@ for (let i = 0; i < acc.length; i++) {
 let modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-let img = document.getElementById("myImg");
+let img = document.getElementsByClassName("myImg");
 let modalImg = document.getElementById("img01");
 let captionText = document.getElementById("caption");
 img.onclick = function () {
